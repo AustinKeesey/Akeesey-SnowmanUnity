fb6359588118c6d2a70c916af05af86a529c12da
AustinKeesey <akeesey2023@chadwickschool.org>
2021-12-03 09:11:03 -0800

    workday

--- Snowman/Assets/GameController.cs
+++ Snowman/Assets/GameController.cs
@@ -1,37 +1,80 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using WordGuesser;
 
 public class GameController : MonoBehaviour
 {
-    public UnityEngine.UI.Text Message;
-    public UnityEngine.UI.Button StartButton;
+    public UnityEngine.UI.Text Message;
+    public UnityEngine.UI.Button StartButton;
+    private WordGame guessingGame;
+    public GameObject StartScreen;
+    public GameObject PlayScreen;
+    public GameObject OverScreen;
+    public GameObject WonScreen;
+    public UnityEngine.UI.InputField PlayerGuess;
+    public UnityEngine.UI.Text HiddenLetters;
+    public UnityEngine.UI.Text LettersGuessed;
+    public UnityEngine.UI.Text RemainingGuess;
+    public UnityEngine.UI.Text CheckGuess;
+    public void StartGame()
+    {
+        this.guessingGame = new WordGame("apple", 5);
+        Debug.Log(this.guessingGame.GetWord());
+        Debug.Log(this.guessingGame.GetFullWord());
+        this.Message.text = "Can you save the Snowman?";
+        this.StartScreen.gameObject.SetActive(false);
+        this.PlayScreen.gameObject.SetActive(true);
+        this.OverScreen.gameObject.SetActive(false);
+        this.WonScreen.gameObject.SetActive(false);
+        this.CheckGuess.text = this.guessingGame.CheckGuess(PlayerGuess.text);
 
-    public GameObject StartScreen;
-    public GameObject PlayScreen;
+        this.HiddenLetters.text = this.guessingGame.GetWord();
 
-    private WordGuesser.WordGame guessingGame;
+        this.LettersGuessed.text = this.guessingGame.GetGuessedLetters();
 
-    public void StartGame()
-    {
-        this.Message.text = "Can You Save The Snowman?";
-        this.StartButton.gameObject.SetActive(true);
-        this.guessingGame = new WordGuesser.WordGame("apple", 5);
-        Debug.Log(this.guessingGame.GetWord());
-        Debug.Log(this.guessingGame.GetFullWord());
+        int remaining = guessingGame.GetGuessLimit() - guessingGame.GetIncorrectGuesses();
 
-        StartScreen.SetActive(false);
-        PlayScreen.SetActive(true);
-        
-    }
-    public void OpenStartScreen()
-    {
-        StartScreen.SetActive(true);
-        PlayScreen.SetActive(false);
-    }
-    public void Start()
-    {
-        StartScreen.SetActive(true);
-        PlayScreen.SetActive(false);
-    }
+        this.RemainingGuess.text = $"You have {remaining} guesses remaining.";
+        PlayerGuess.text = string.Empty;
+
+
+        
+    }
+
+    public void Start()
+    {
+        this.StartScreen.gameObject.SetActive(true);
+
+        this.PlayScreen.gameObject.SetActive(false);
+
+        this.OverScreen.gameObject.SetActive(false);
+
+        this.WonScreen.gameObject.SetActive(false);
+    }
+   
+    public void SubmitGuess()
+    {
+        this.CheckGuess.text = this.guessingGame.CheckGuess(PlayerGuess.text);
+
+        this.HiddenLetters.text = this.guessingGame.GetWord();
+
+        this.LettersGuessed.text = this.guessingGame.GetGuessedLetters();
+
+        int remaining = guessingGame.GetGuessLimit() - guessingGame.GetIncorrectGuesses();
+
+        this.RemainingGuess.text = $"You have {remaining} guesses remaining.";
+        PlayerGuess.text = string.Empty;
+        if (this.guessingGame.IsGameOver())
+        {
+            this.PlayScreen.gameObject.SetActive(false);
+            this.OverScreen.gameObject.SetActive(true);
+        }
+        if (this.guessingGame.IsGameWon())
+        {
+            this.PlayScreen.gameObject.SetActive(false);
+            this.WonScreen.gameObject.SetActive(true);
+        }
+
+    }
 }
